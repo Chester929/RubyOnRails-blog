2229410bdb93c10415f7210df1b30816b8ab0fb0
Chester929 <chester929@gmail.com>
2015-09-24 00:45:19 +0200

    Added

--- articles/app/controllers/articles_controller.rb
+++ articles/app/controllers/articles_controller.rb
@@ -1,21 +1,53 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_post, only: [:show, :destroy, :edit, :update]
+  #pred akoukolvek akciy z vyhradenych metod(show, destroy, edit a update), vykonaj metodu set_post.
+
   def index
-    #@articles = Article.all
+    @articles = Article.all
   end
 
   def new
-    #@article= Article.new
+    @article= Article.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to articles_path, notice: 'Article was deleted'
+    else
+      render :show
+    end
   end
 
   def create
       @article= Article.new(article_params)
 
-      @article.save
-      redirect_to @article
+      if @article.save
+        redirect_to @article, notice: 'Article was added'
+      else
+        render :new
+      end
   end
 
   private
+
+  def set_post
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :body)
